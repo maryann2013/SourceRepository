8e8211696e9579f5a4bd08e0bb36c570ae98a144
ashap <ashap@LAPTOP-KMBS6HN9>
2019-12-11 13:38:15 +0400

    first commit... uploading all

---
+++ MyFirstJavaPoject/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ashap
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.print("Hello World");
+	}
+
+}
